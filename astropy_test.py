51b598bc1afd00db794786dc9f138d2c84283a4a
armours <psds075@gmail.com>
2018-06-03 14:40:28 +0900

    Alpha Version

---
+++ astropy_test.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+from PIL import Image
+from astropy.io import fits
+import img_scale
+#from astropy.utils.data import download_file
+#image_file = download_file('http://data.astropy.org/tutorials/FITS-images/HorseHead.fits', cache=True )
+
+dir_path = os.getcwd()
+for filename in os.listdir(dir_path+"\\data"):
+    # If the images are not .JPG images, change the line below to match the image type.
+    if filename.endswith(".fits"):
+        image_data = fits.getdata("data\\"+filename)
+        if len(image_data.shape) == 2:
+            sum_image = image_data
+        else:
+            sum_image = image_data[0] - image_data[0]
+            for single_image_data in image_data:
+                sum_image += single_image_data  
+
+        sum_image = img_scale.sqrt(sum_image, scale_min=0, scale_max=np.amax(image_data))
+        sum_image = sum_image * 200
+        im = Image.fromarray(sum_image)
+        #im = Image.fromarray(image_data[0]+image_data[1]+image_data[2])
+        if im.mode != 'RGB':
+            im = im.convert('RGB')
+    
+        im.save(dir_path+"\\image\\"+filename+".jpg")
+        im.close()
